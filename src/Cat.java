cdc74522020c0c8a98be02ac9e38ddf9c7ca6175
Carnecchiad <league@iMac.attlocal.net>
2018-02-04 14:27:37 -0800

    loud dog

--- src/Cat.java
+++ src/Cat.java
@@ -1,4 +1,12 @@
 
-public class Cat {
-
+public class Cat extends Pet{
+String cat_sound;
+	
+	public Cat(String petName) {
+		super (petName);
+	}
+	
+	public String speak(){
+		return cat_sound;
+	}
 }
---
+++ src/LoudDog.java
@@ -0,0 +1,9 @@
+
+public class LoudDog extends Dog{
+
+	public LoudDog(String petName) {
+		super(petName);
+		// TODO Auto-generated constructor stub
+	}
+
+}
